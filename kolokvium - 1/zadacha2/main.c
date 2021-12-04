a332a449673206eade8a7cc80701ee19796410f5
filipmazev <fmazev@yahoo.com>
2021-12-04 19:41:50 +0100

    kolokviumski zadaci resenija - termin 1
    //

---
+++ kolokvium - 1/zadacha2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int x, n; scanf("%d %d", &x, &n);
+
+    while(n)
+    {
+        int number; scanf("%d", &number); if(number>9){
+        int div = pow(10,((int)(number==0?1:log10(number)+1))-1), digit = (number/div) % 10;
+        if((((number - (digit*div)) * 10) + digit) % x == 0){ printf("DA\n"); } else { printf("NE\n"); } } n--;
+    }
+
+    return 0;
+}
