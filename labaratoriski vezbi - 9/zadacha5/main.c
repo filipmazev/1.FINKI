b44780042a5c2119688c7d6595691e2b738670c8
filipmazev <fmazev@yahoo.com>
2021-12-09 23:38:57 +0100

    labs9 - coming soon

--- labaratoriski vezbi - 9/zadacha5/main.c
+++ labaratoriski vezbi - 9/zadacha5/main.c
@@ -3,11 +3,11 @@
 
 void letterFrequency (char *text, char letter)
 {
-    int i=0; int cnt_lower=0, cnt_upper=0;
+    unsigned int i=0, cnt_lower=0, cnt_upper=0;
 
-    while(*(text+(int)i) != '\n'){
-    if(isalpha(*(text+(int)i))){ if(tolower(letter) == *(text+(int)i)){ cnt_lower++; }
-    if(toupper(letter) == *(text+(int)i)){ cnt_upper++; } } i++; }
+    while(*(text+i) != '\n'){
+    if(isalpha(*(text+i))){ if(tolower(letter) == *(text+i)){ cnt_lower++; }
+    if(toupper(letter) == *(text+i)){ cnt_upper++; } } i++; }
 
     printf("%c -> %.3f%%\n", tolower(letter), (cnt_lower/(float)i)*100);
     printf("%c -> %.3f%%\n", toupper(letter), (cnt_upper/(float)i)*100);
@@ -20,7 +20,5 @@ int main()
     while(character != '\n'){ scanf("%c", &character); *(text+i) = character; i++;
     if(i>size){ size*=2; void *temp = realloc(text, size * sizeof(char)); text = temp; } }
 
-    char letter; scanf("%c", &letter); letterFrequency(text, letter);
-
-    return 0;
+    char letter; scanf("%c", &letter); letterFrequency(text, letter); return 0;
 }
