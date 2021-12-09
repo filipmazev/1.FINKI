aa6c3ffb32f9f776106176beba03d63399b0a180
filipmazev <fmazev@yahoo.com>
2021-12-09 20:14:33 +0100

    labs9 - coming soon

---
+++ labaratoriski vezbi - 9/zadacha5/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void test(char *text, char letter)
+{
+    int i=0; int cnt_lower=0, cnt_upper=0;
+
+    while(*(text+(int)i) != '\n'){
+    if(isalpha(*(text+(int)i))){ if(tolower(letter) == *(text+(int)i)){ cnt_lower++; }
+    if(toupper(letter) == *(text+(int)i)){ cnt_upper++; } } i++; }
+
+    printf("%d %d\n", cnt_lower, i);
+    printf("%c -> %.3f%%\n", tolower(letter), (cnt_lower/(float)i)*100);
+    printf("%c -> %.3f%%\n", toupper(letter), (cnt_upper/(float)i)*100);
+}
+
+int main()
+{
+    size_t size = 1024; char *text = calloc(size, sizeof(char)); unsigned int i=0; char character=' ';
+
+    while(character != '\n'){ scanf("%c", &character); *(text+i) = character; i++;
+    if(i>size){ size*=2; void *temp = realloc(text, size * sizeof(char)); text = temp; } }
+
+    char letter; scanf("%c", &letter); test(text, letter);
+
+    return 0;
+}
