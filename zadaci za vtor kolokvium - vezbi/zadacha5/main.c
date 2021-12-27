a5f761285a26dce6e9309bf4c676986d7fc741d2
filipmazev <fmazev@yahoo.com>
2021-12-27 15:01:34 +0100

    release

--- zadaci za vtor kolokvium - vezbi/zadacha5/main.c
+++ zadaci za vtor kolokvium - vezbi/zadacha5/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void wf()
 {
@@ -8,35 +9,34 @@ void wf()
     fclose(f);
 }
 
-void reallocate(char *string, size_t size, size_t var){ void *temp = realloc(string, size * var); string = temp; }
-
-int main()
+int main(void)
 {
-    wf(); FILE *read = fopen ("livce.txt", "r");
-    size_t size=1024; char *string = malloc(size * sizeof(char));
-    unsigned int i=0; int cnt=0, flag=0, cnt_pos=0, pos, money;
-    double *Arr = malloc(size * sizeof(double)), winnings=0, max=0, coeff=1, mult=0, value=0;
+    int cnt_r=0, cnt_l=0, size_l=0, i=0, pos=0; double total=1,max=0;
 
-    while(!feof(read))
+    wf(); FILE *read_asses = fopen("livce.txt", "r");
+    while(!feof(read_asses)){ if(fgetc(read_asses) == '\n'){ cnt_r++; if(cnt_l>size_l){ size_l=cnt_l; } cnt_l=0; } cnt_l++; }
+
+    fclose(read_asses); FILE *read = fopen("livce.txt", "r"); char *line[cnt_r][size_l];
+    while(fgets(line[i], size_l+1, read)){ line[i][strlen(line[i]) - 1] = '\0'; i++; }
+
+    char *temp, string_temp[size_l+1]; double value = strtod(line[0], &temp);
+
+    for(int j=1; j<i; j++)
     {
-        char character = fgetc(read);
-        if(i>=size){ size*=2; reallocate(string, size, sizeof(char)); reallocate(Arr, size, sizeof(double)); }
-        if(character == ' ' || character == '\n' || character == '\0')
+        char string[size_l+1]; strcpy(string, ""); strcpy(string, line[j]);
+        for(int k=0; k<strlen(string); k++)
         {
-            free(string); string = malloc(size * sizeof(char));
-            if(!flag){ flag=1; cnt=-1; money = value; }
-            else if(cnt==2) { coeff*=*(Arr+cnt); winnings = money * coeff;
-            if(winnings>max){ max=winnings; } if(value>mult) { mult=value; pos=cnt_pos; } }
-            if(character == '\n'){ cnt_pos++; } i=0; cnt++; if(cnt>2){ cnt=0; }
+            strncat(string_temp, &string[k], 1);
+            if(string[k] == ' ' || string[k] == '\0' || string[k] == '\n')
+            {
+                char *temp_2; double mult = strtod(string_temp, &temp_2);
+                char value_save[size_l+1]; snprintf(value_save, size_l+1, "%.2lf", mult);
+
+                if(k>=10){ if(mult>max){ max=mult; pos=j; } total*=mult; }
+                strcpy(string_temp, "");
+            }
         }
-        else { *(string+i) = character; i++; char *temp; value = strtod(string, &temp); } *(Arr+cnt) = value;
     }
 
-    cnt_pos=0; fclose(read); FILE *read_again = fopen ("livce.txt", "r");
-
-    while(!feof(read_again)){
-        char character = fgetc(read_again); if(cnt_pos==pos){ printf("%c", character); }
-        if(character == '\n'){ cnt_pos++; } }
-
-    printf("%.2lf", winnings); return 0;
+    printf("%s", line[pos]); printf("%.2lf", value*total); return 0;
 }
