f9b1670609088523b5c919cd6ccce5012cdf79cc
filipmazev <fmazev@yahoo.com>
2021-12-27 01:23:00 +0100

    labs update

--- zadaci za vtor kolokvium - vezbi/zadacha2/main.c
+++ zadaci za vtor kolokvium - vezbi/zadacha2/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <math.h>
+#include <ctype.h>
+
+int pow_div(int n){ if(n<10){ return 1; } return 10 * pow_div(n/10); }
 
 void wtf()
 {
@@ -10,14 +12,14 @@ void wtf()
 
 int main()
 {
-    wtf(); FILE *read = fopen("broevi.txt", "r");
-    int number=0, cnt=0, max=0, max_number=0;
+    wtf(); FILE *read = fopen("broevi.txt", "r"); int number=0, cnt=0, max=0, max_number=0, num_org;
 
     while(!feof(read))
     {
-        char character = fgetc(read); if(isdigit(character)){number = number * 10 + character - '0'; }
-        else{ if(cnt){ int digit = (number/(int)(pow(10,((int)((number==0)?1:log10(number))))));
+        char character = fgetc(read);
+        if(isdigit(character)){ number = number * 10 + character - '0'; num_org=number; }
+        else{ if(cnt){ int digit = (number/pow_div(number));
                 if(digit > max){ max=digit; max_number = number; } } number=0; cnt++; }
-        if(character == '\n'){ printf("%d \n", max_number); cnt=0; max=0; }
+        if(character == '\n' && num_org){ printf("%d \n", max_number); cnt=0; max=0; }
     } return 0;
 }
