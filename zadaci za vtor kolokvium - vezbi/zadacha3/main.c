919f199552c18ab0f7ff1b523a571c75d0355522
filipmazev <fmazev@yahoo.com>
2022-01-15 17:06:10 +0100

    minor update

--- zadaci za vtor kolokvium - vezbi/zadacha3/main.c
+++ zadaci za vtor kolokvium - vezbi/zadacha3/main.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum_pos(int *arr, int n, int ind)
+int sum_pos(int *array,int n, int m)
 {
-    int sum=0; for(int i=ind; i<n; i++){ sum+=*(arr+i); }
-    return sum;
+    if(m == n){ return 0; }
+    return *(array+m) + sum_pos(array, n, m+1);
 }
 
 int main()
 {
-    int *arr, n=101, ind=-1, sum=0;
+    int n, m; scanf("%d",&n); int array[n];
+    for(int i=0;i<n;i++){ scanf("%d",&*(array+i)); }
 
-    while(n>100){ scanf("%d", &n); }
-    for(int i=0; i<n; i++){ scanf("%d", &*(arr+i)); }
-
-    while(ind<0){ scanf("%d", &ind); } if(ind>n){ return 0; }
-    printf("%d", sum_pos(arr, n, ind)); return 0;
+    scanf("%d",&m); printf("%d", sum_pos(array,n,m)); return 0;
 }
