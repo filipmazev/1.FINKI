98904d66fb08e3b285f2334a3b44bc074435da24
filipmazev <fmazev@yahoo.com>
2022-01-21 00:52:31 +0100

    FEIT

--- vtor kolokvium - vezbi/zadacha3/main.c
+++ vtor kolokvium - vezbi/zadacha3/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void *remove_compare(char *word, char *comp)
 {
@@ -10,19 +11,15 @@ void *remove_compare(char *word, char *comp)
 
 int main()
 {
-    size_t size=1024; char *string = calloc(size, sizeof(char)); unsigned int i=0, j=0; char ch;
+    char string[1024]; unsigned int i=0, size=0; char ch;
+    while((ch = getchar()) != '\n'){ string[i] = ch; i++; }
 
-    while((ch = getchar()) != '\n'){
-    if(i>=size){ size*=2; char *temp = realloc(string, size * sizeof(char)); string = temp; } *(string+i) = ch; i++; }
+    char *word = strtok(string, " "); char **str_arr = calloc(1024, sizeof(char));
+    while(word!=NULL){ str_arr[size] = calloc(strlen(word), sizeof(char)); strcpy(str_arr[size], word); word = strtok(NULL, " "); size++; }
 
-    char *word; word = strtok(string, " "); size_t lenght=size; char **str_arr = calloc(lenght, sizeof(char));
-
-    while(word!=NULL){ if(j>=lenght){ lenght*=2; char **temp = realloc(str_arr, lenght * sizeof(char)); str_arr = temp; }
-        str_arr[j] = calloc(strlen(word), sizeof(char)); strcpy(str_arr[j], word); word = strtok(NULL, " "); j++; }
-
-    for(int pos=0; pos<j/2; pos++){
+    for(int pos=0; pos<size/2; pos++){
         char temp[strlen(str_arr[pos])]; strcpy(temp, str_arr[pos]); remove_compare(temp, "aeiou");
-            if(!strcmp(temp, str_arr[j-pos-1])){ printf("%s-%s\n",str_arr[pos], str_arr[j-pos-1]); } }
+            if(!strcmp(temp, str_arr[size-pos-1])){ printf("%s-%s\n",str_arr[pos], str_arr[size-pos-1]); } }
 
     return 0;
 }
