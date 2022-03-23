ddcae0c6aac8dbb0132a827c4a13753af7286fec
filipmazev <fmazev@yahoo.com>
2022-03-23 13:46:16 +0100

    //

--- OOP/1.other/vector functions replication in class - classes - FEIT/main.cpp
+++ OOP/1.other/vector functions replication in class - classes - FEIT/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#define MAX 20
+#define MAX 1024
 
-struct _stack
+struct stack
 {
     int *arr=nullptr, num;
 
-    _stack init(){ this->num=0; this->arr = new int[this->num]; }
+    stack init(){ this->num=0; this->arr = new int[this->num]; }
 
     void push(int element)
     {
@@ -13,7 +13,7 @@ struct _stack
         int *tmp = new int[this->num+1];
         for(int i=0; i<this->num; i++){ tmp[i] = this->arr[i]; }
         tmp[this->num++]=element; delete[] this->arr; this->arr = tmp; }
-        else { std::cout<<"Magacinot e poln"<<std::endl; }
+        else { std::cout<<std::endl<<"ERROR 01 | the stack is full"<<std::endl; }
     }
 
     void pop()
@@ -22,14 +22,7 @@ struct _stack
         int *tmp = new int[--this->num];
         for(int i=0; i<this->num; i++){ tmp[i] = this->arr[i]; }
         delete[] this->arr; this->arr = tmp; }
-        else { std::cout<<"Magacinot e prazen"<<std::endl; }
-    }
-
-    _stack &operator = (_stack &adder)
-    {
-        this->num = adder.size(); delete[] this->arr; this->arr = new int[num];
-        for(int i=0; i<num; i++){ this->arr[i] = adder.arr[i]; }
-        return *this;
+        else { std::cout<<std::endl<<"ERROR 02 | the stack is empty"<<std::endl; }
     }
 
     int size(){ return num; }
@@ -39,9 +32,9 @@ struct _stack
     void print(){ for(int i=0; i<this->num; i++){ std::cout<<this->arr[i]; } }
 };
 
-void func(_stack &s)
+void func(stack &s)
 {
-    _stack tmp; tmp.init();
+    stack tmp; tmp.init();
     for(int i=0; i<s.size(); i++)
     { int sum = s.arr[i] + s.arr[s.size()-i-1]; sum=sum>9?9:sum; tmp.push(sum); }
     s = tmp;
@@ -49,8 +42,8 @@ void func(_stack &s)
 
 int main()
 {
-    _stack s; int no, element; s.init();
-    std::cout<<"Vnesete koj broj da se proveri:"; std::cin>>no;
+    stack s; int no, element; s.init();
+    std::cout<<"Enter a number: "; std::cin>>no;
 
     while(no!=0){ element = no%10; no/=10; s.push(element); }
     func(s); s.print(); return 0;
